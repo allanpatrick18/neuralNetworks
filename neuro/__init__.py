b1d64c3dd76bebfe9e38612d4707ec1dfb6b7ca2
allan_noot <allanpatrick18@gmail.com>
2018-04-04 00:28:43 -0300

    clear project

--- neuro/__init__.py
+++ neuro/__init__.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+from neuro.perceptron import perceptron
+
+if __name__ == '__main__':
+    X = np.array([
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1]
+    ])
+    d = np.array([0, 0, 0, 1])
+
+    perceptro = perceptron(input_size=2)
+    perceptro.fit(X, d)
+    print(perceptro.W)
