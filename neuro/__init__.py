9c9ba01337d7a8936f2401d1e9f4798ea00a93e6
allan_noot <allanpatrick18@gmail.com>
2018-04-04 21:52:29 -0300

    Perceptron fininalized with one graph

--- neuro/__init__.py
+++ neuro/__init__.py
@@ -9,8 +9,53 @@ if __name__ == '__main__':
         [1, 0],
         [1, 1]
     ])
-    d = np.array([0, 0, 0, 1])
+    result_and = np.array([0, 0, 0, 1])
+    result_or = np.array([0, 1, 1, 1])
 
-    perceptro = perceptron(input_size=2)
-    perceptro.fit(X, d)
-    print(perceptro.W)
+
+    while(1):
+        print("=================")
+        print("1-Porta AND Padrão")
+        print("2-Porta AND Batelada")
+        print("3-Porta OR Padrão")
+        print("4-Porta OR Batelada")
+        q = int(input())
+
+        if q == 1:
+            print("Porta AND Padrão")
+            port_and = perceptron(input_size=2)
+            port_and.fit(X, result_and)
+            port_and.printTrain(X, result_and)
+            port_and.graph_err()
+            print(port_and.W)
+
+        elif q == 2:
+            print("==========")
+            print("2-Porta AND Batelada")
+            port_and = perceptron(input_size=2)
+            port_and.batelada(X, result_and)
+            port_and.printTrain(X, result_and)
+            port_and.graph_err()
+            print(port_and.W)
+
+        elif q == 3:
+            print("3-Porta OR Padrão")
+            print("==========")
+            port_or = perceptron(input_size=2)
+            port_or.fit(X, result_or)
+            port_or.printTrain(X, result_or)
+            port_and.graph_err()
+            print(port_or.W)
+        elif q == 4:
+            print("4-Porta OR Batelada")
+            print("==========")
+            port_or = perceptron(input_size=2)
+            port_or.batelada(X, result_or)
+            port_or.printTrain(X, result_or)
+            port_and.graph_err()
+            print(port_or.W)
+
+        else:
+            nome = None
+            print("Valor invalido")
+
--- neuro/perceptron.py
+++ neuro/perceptron.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class perceptron(object):
 
@@ -8,6 +8,7 @@ class perceptron(object):
         #add one for bias
         self.epochs = epochs
         self.lr = lr
+        self.erros_gragh = []
 
     def activation_fn(self, x):
         #return (x >= 0).astype(np.float32)
@@ -19,9 +20,41 @@ class perceptron(object):
         return a
 
     def fit(self, X, d):
-        for _ in range(self.epochs):
+        for k in range(self.epochs):
             for i in range(d.shape[0]):
                 x = np.insert(X[i], 0, 1) #insert 1 pos em 0 [1,X[i]]
                 y = self.predict(x)
                 e = d[i] - y
+                self.erros_gragh.append(e)
                 self.W = self.W + self.lr * e * x
+
+    def batelada(self, X, d):
+        sum_err=0
+        for k in range(self.epochs):
+            for i in range(d.shape[0]):
+                x = np.insert(X[i], 0, 1) #insert 1 pos em 0 [1,X[i]]
+                y = self.predict(x)
+                e = d[i] - y
+                sum_err +=e*x
+                self.erros_gragh.append(e)
+            self.W = self.W + self.lr * sum_err
+
+    def printTrain(self,X,d):
+        for i in range(d.shape[0]):
+            x = np.insert(X[i], 0, 1)  # insert 1 pos em 0 [1,X[i]]
+            y = self.predict(x)
+            print(str(X[i])+" = "+str(y))
+
+    def graph_err(self):
+        plt.figure()
+        plt.plot(self.erros_gragh)
+        plt.xscale('log')
+        plt.xlabel('Epocas log')
+        plt.ylabel('Valor do erro')
+        plt.title('Erros durante treinamento')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+
+
